42c9d6c55930c9c09078fd22859b87e9ea045c95
Hakyung Sohn <hortenssiaa@gmail.com>
2025-05-12 00:48:17 +0900

    setting: #1 - 프로젝트 세팅 w/ Clean Architecture

---
+++ BookFinder/BookFinder/Presentation/Scene/SearchTap/ViewController/SearchTapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  BookFinder
+//
+//  Created by kingj on 5/11/25.
+//
+
+import UIKit
+
+final class SearchTapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
