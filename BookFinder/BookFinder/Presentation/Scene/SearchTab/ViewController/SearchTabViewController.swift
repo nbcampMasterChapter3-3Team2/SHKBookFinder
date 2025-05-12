4d18a746145888982cf7214a8061a088022a34d8
Hakyung Sohn <hortenssiaa@gmail.com>
2025-05-12 14:45:19 +0900

    design: #3 - Search Bar UI

--- BookFinder/BookFinder/Presentation/Scene/SearchTab/ViewController/SearchTabViewController.swift
+++ BookFinder/BookFinder/Presentation/Scene/SearchTab/ViewController/SearchTabViewController.swift
@@ -6,13 +6,37 @@
 //
 
 import UIKit
+import Then
+import SnapKit
 
 final class SearchTabViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    private lazy var searchBar = UISearchBar().then {
+        $0.delegate = self
+        $0.placeholder = "입력"
+        $0.searchBarStyle = .minimal
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+
+    private func configureUI() {
+        [
+            searchBar
+        ]
+            .forEach { view.addSubview($0) }
+
+        searchBar.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.directionalHorizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(10)
+        }
+    }
 }
 
+extension SearchTabViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("input: \(searchText)")
+    }
+}
